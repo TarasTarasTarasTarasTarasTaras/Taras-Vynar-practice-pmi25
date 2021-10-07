f3a1ffe609e6033aafd67dfcef7c4ac2d827ffb7
Taras Vynar <90195508+TarasTarasTarasTarasTarasTaras@users.noreply.github.com>
2021-10-07 21:55:36 +0300

    Update Iterator.py

--- Iterator.py
+++ Iterator.py
@@ -1,8 +1,8 @@
 import random
-import functools
 
 class IteratorLL:
-    def __init__(self, size, function = functools.partial(random.randint, 0, 10)):
+    def __init__(self, size, *args, function = random.randint):
+        self.__args = args
         self.__curr_index = 0
         self.__size = size
         self.__function = function
@@ -14,9 +14,8 @@ class IteratorLL:
     def __next__(self):
         if self.__curr_index >= self.__size:
             raise StopIteration
-        value = self.__function()
         self.__curr_index += 1
-        return value
+        return self.__function(*self.__args)
 
 
 
