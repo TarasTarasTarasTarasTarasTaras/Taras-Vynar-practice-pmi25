c9f7f61e4218450cb5554eff1f15b8534642fe1b
Taras Vynar <90195508+TarasTarasTarasTarasTarasTaras@users.noreply.github.com>
2021-11-18 02:56:15 +0200

    Add files via upload

---
+++ authorization/managers.py
@@ -0,0 +1,45 @@
+import jwt
+
+from datetime import datetime, timedelta
+
+from django.conf import settings 
+from django.contrib.auth.models import (
+	AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+
+from django.db import models
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **names):
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        if password is None:
+            raise TypeError('Users must have a password.')
+        email=self.normalize_email(email)
+        user = self.model(email=self.normalize_email(email), **names)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, _first_name, _last_name, email, password):
+        """ Создает и возввращет пользователя с привилегиями суперадмина. """
+        if _first_name is None:
+            raise TypeError('Users must have a first name')
+        
+        if _last_name is None:
+            raise TypeError('Users must have a last name')
+        
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        if password is None:
+            raise TypeError('Users must have a password.')
+
+        user = self.model(first_name = _first_name, last_name = _last_name, email=self.normalize_email(email))
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
